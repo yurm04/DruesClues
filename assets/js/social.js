8e19e462e297ab1849fee73c437db00d7e0d53d9
Yuraima <yurm04@gmail.com>
2015-11-23 00:01:40 -0500

    trimmed down and added comming soon

---
+++ assets/js/social.js
@@ -0,0 +1,12 @@
+/* ---------------------
+    social.js
+
+    Controls social icons in nav-bar
+--------------------- */
+
+(function() {
+    links = document.getElementsByClassName('social-link');
+
+    
+
+})()
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
+// compile and compress sass files
 gulp.task('sass', function() {
-    gulp.src('sass/**/*.scss')
+    gulp.src('assets/sass/**/*.scss')
         .pipe(sass({outputStyle : 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
 });
 
+// concat javascript files
+gulp.task('js', function() {
+    gulp.src('assets/js/**/*.js')    
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('./js/'));
+})
+
+// watch for file changes
 gulp.task('watch', function() {
-    gulp.watch('sass/**/*.scss',['sass']);
+    gulp.watch('assets/sass/**/*.scss',['sass']);
+    gulp.watch('assets/js/**/*.js',['js']);
 });
 
-//Watch task
-gulp.task('default', ['sass', 'watch']);
+gulp.task('default', ['sass', 'js', 'watch']);
 
